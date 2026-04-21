6056cddfd9c7f950bd5b1d93eb25b725d8d72a06
alrosi12 <91032271+alrosi12@users.noreply.github.com>
2026-04-21 14:33:08 +0300

    Controllers & Routes

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Enums\UserGoal;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
@@ -30,6 +31,10 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'weight' => fake()->randomFloat(2, 55, 120),
+            'height' => fake()->randomFloat(2, 155, 200),
+            'birth_date' => fake()->dateTimeBetween('-55 years', '-18 years')->format('Y-m-d'),
+            'goal' => fake()->randomElement(collect(UserGoal::cases())->map->value->all()),
         ];
     }
 
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,64 @@
 <?php
 
+use App\Http\Controllers\BodyMeasurementController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ExerciseController;
+use App\Http\Controllers\FriendshipController;
+use App\Http\Controllers\ProfileCompletionController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProgressController;
+use App\Http\Controllers\PublicProfileController;
+use App\Http\Controllers\SessionSetController;
+use App\Http\Controllers\WorkoutPlanController;
+use App\Http\Controllers\WorkoutSessionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/workout-plans/share/{token}', [WorkoutPlanController::class, 'shareShow'])
+    ->name('workout-plans.share');
+
+Route::get('/profiles/{user}', [PublicProfileController::class, 'show'])
+    ->name('profiles.show');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/complete-profile', [ProfileCompletionController::class, 'show'])
+        ->name('profile.complete.show');
+    Route::patch('/complete-profile', [ProfileCompletionController::class, 'update'])
+        ->name('profile.complete.update');
+});
+
+Route::middleware(['auth', 'verified', 'profile.complete'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+    Route::get('/progress', [ProgressController::class, 'index'])->name('progress.index');
+
+    Route::post('workout-plans/{workout_plan}/duplicate', [WorkoutPlanController::class, 'duplicate'])
+        ->name('workout-plans.duplicate');
+    Route::post('workout-plans/{workout_plan}/share', [WorkoutPlanController::class, 'enableShare'])
+        ->name('workout-plans.share.enable');
+    Route::delete('workout-plans/{workout_plan}/share', [WorkoutPlanController::class, 'disableShare'])
+        ->name('workout-plans.share.disable');
+
+    Route::resource('exercises', ExerciseController::class);
+    Route::resource('workout-plans', WorkoutPlanController::class);
+    Route::resource('workout-sessions', WorkoutSessionController::class);
+    Route::post('workout-sessions/{workout_session}/finish', [WorkoutSessionController::class, 'finish'])
+        ->name('workout-sessions.finish');
+    Route::resource('workout-sessions.session-sets', SessionSetController::class);
+    Route::resource('body-measurements', BodyMeasurementController::class);
+
+    Route::post('friendships/{friendship}/accept', [FriendshipController::class, 'accept'])
+        ->name('friendships.accept');
+    Route::post('friendships/{friendship}/reject', [FriendshipController::class, 'reject'])
+        ->name('friendships.reject');
+    Route::resource('friendships', FriendshipController::class);
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
